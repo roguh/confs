05cf518a2d511e97bccefeecb1aeacae3daa3775
Hugo Rivera <hugo.rivera2319@gmail.com>
2016-09-15 23:29:10 -0600

    Backup confs before copying.

--- update.sh
+++ update.sh
@@ -6,33 +6,54 @@ fi
 echo $1 to $2
 SRC_DIR=$1
 DST_DIR=$2
+BACKUP_DIR=./confs-backup
+
+echo Backup directory in $BACKUP_DIR
 
 echo Press ENTER to copy configuration files from source directory $SRC_DIR to $DST_DIR
 read a 
 
-# vim
-mkdir -p $DST_DIR/.vim/autoload
-cp $SRC_DIR/.vimrc $DST_DIR/.vimrc
-cp $SRC_DIR/.vim/autoload/plug.vim $DST_DIR/.vim/autoload/plug.vim
+mkdir_conf() {
+    echo mkdir -p $DST_DIR/$1
+    mkdir -p $DST_DIR/$1
+    mkdir -p $BACKUP_DIR/$1
+}
 
-# emacs.d
-mkdir -p $DST_DIR/.emacs.d
-cp $SRC_DIR/.emacs.d/init.el $DST_DIR/.emacs.d/init.el
-cp $SRC_DIR/.emacs.d/ui.el   $DST_DIR/.emacs.d/ui.el
+copy_conf() {
+    cp $SRC_DIR/$1 $BACKUP_DIR/$1
+    echo cp $SRC_DIR/$1 $DST_DIR/$1
+    cp $SRC_DIR/$1 $DST_DIR/$1
+}
 
-# i3
-mkdir -p $DST_DIR/.config/i3
-cp $SRC_DIR/.i3status.conf         $DST_DIR/.i3status.conf
-cp $SRC_DIR/.config/i3/config      $DST_DIR/.config/i3/config
-cp $SRC_DIR/.config/i3/config.base $DST_DIR/.config/i3/config.base
-cp $SRC_DIR/.config/i3/startup.sh  $DST_DIR/.config/i3/startup.sh
+echo vim
+mkdir_conf .vim/autoload
+copy_conf .vimrc
+copy_conf .vim/autoload/plug.vim
+
+echo TODO: may need to run 'ln -s ~/.vim ~/.config/nvim'
+echo TODO: may need to run 'ln -s ~/.vimrc ~/.config/nvim/init.vim'
+
+echo emacs.d
+mkdir_conf .emacs.d
+copy_conf .emacs.d/init.el
+
+echo i3
+mkdir_conf .config/i3
+copy_conf .i3status.conf
+copy_conf .config/i3/config
+copy_conf .config/i3/config.base
+copy_conf .config/i3/startup.sh
 
 echo TODO: may need to run "git clone https://github.com/okraits/j4-make-config"
 
-# zsh
-cp $SRC_DIR/.zshrc $DST_DIR/.zshrc
+echo bash
+copy_conf .bashrc 
+
+echo zsh
+copy_conf .zshrc
+
 echo TODO: may need to run "git clone https://github.com/robbyrussell/oh-my-zsh"
 
-# terminator
-mkdir -p $DST_DIR/.config/terminator
-cp $SRC_DIR/.config/terminator/config $DST_DIR/.config/terminator/config
+echo terminator
+mkdir_conf .config/terminator
+copy_conf .config/terminator/config
