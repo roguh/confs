b31fad5daeb270415bff51697476c2102e98dae0
Hugo Rivera <6373447+roguh@users.noreply.github.com>
2018-02-22 19:36:46 -0700

    Colors. Locker

--- i3/bin/locker.sh
+++ i3/bin/locker.sh
@@ -1,3 +1,4 @@
 # -sample is faster than -scale or -resize
-scrot ~/tmp/s.png && mogrify -sample 50% -spread 5 -sample 200% ~/tmp/s.png && \
-i3lock --show-failed-attempts --show-failed-attempts --image=$HOME/tmp/s.png
+scrot ~/tmp/s.png && \
+mogrify -sample 20% -flip -spread 2 -sample 500% -paint 2 ~/tmp/s.png && \
+i3lock --show-failed-attempts --image=$HOME/tmp/s.png
