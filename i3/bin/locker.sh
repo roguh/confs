6c13892205773be2ff94524b9c834df75686aaff
Hugo Rivera <6373447+roguh@users.noreply.github.com>
2018-04-12 21:46:39 -0600

    Updates

--- i3/bin/locker.sh
+++ i3/bin/locker.sh
@@ -1,4 +1,4 @@
 # -sample is faster than -scale or -resize
 scrot ~/tmp/s.png && \
-mogrify -sample 50% -colorspace Gray -spread 1 -sample 200% -paint 3 ~/tmp/s.png && \
+mogrify -sample 10% -colorspace Gray -spread 2 -sample 1000% -paint 6 ~/tmp/s.png && \
 i3lock --show-failed-attempts --image=$HOME/tmp/s.png
