ceb3a43ca1dd9fa87ee7339f16e3e836ec0a2e93
Hugo Rivera <6373447+roguh@users.noreply.github.com>
2018-09-01 16:16:26 -0600

    Improve git support

---
+++ git/bin/gitdiff.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+C=$(stty size | cut -d' ' -f2)
+D=$((C / 2))
+
+echo
+echo
+printf "%${D}s\n" | tr " " "-"
+echo "$1"
+printf "%${D}s\n" | tr " " "v"
+echo
+echo
+
+if command -v icdiff > /dev/null ; then
+    # LOL
+    icdiff --cols=$((D + D/2 + D/8)) --no-headers --line-numbers "$2" "$5" | grep -v -e '^$'
+elif command -v sdiff > /dev/null ; then
+    sdiff --suppress-common-lines  --ignore-all-space --width=$C "$2" "$5"
+else
+    diff --suppress-common-lines -y "$2" "$5"
+fi
+
+# Don't die
+true
+
