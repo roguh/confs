adcd51c4f5ffb754a2353154ef71c95f61516fcf
Hugo Rivera <6373447+roguh@users.noreply.github.com>
2018-09-02 15:14:10 -0600

    Updates. Upload current pywal colors.

--- i3/bin/terminal.sh
+++ i3/bin/terminal.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-( optirun alacritty || alacritty )
+exec alacritty
--- pywal/bin/wal-set-theme.sh
+++ pywal/bin/wal-set-theme.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-wal -o dunst.sh -o test-notifications.sh -o launcher.sh --theme random_dark # -i /home/user/Photos/minimal-backgrounds/Intuition 2.png
+wal -o $HOME/py/polybar-themer/use_pywal_theme.py -o dunst.sh -o launcher.sh --theme random_dark # -i /home/user/Photos/minimal-backgrounds/Intuition 2.png
