8c5f70bb75aa2d2b73220a508e48c036ce234521
Hugo Rivera <6373447+roguh@users.noreply.github.com>
2018-09-12 12:52:01 -0600

    Update pywal colors

--- pywal/bin/wal-set-theme.sh
+++ pywal/bin/wal-set-theme.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 # THEME=base16-greenscreen
 # THEME=base16-zenburn
-# THEME=base16-black-metal-burzum
-# THEME=base16-black-metal-nile
 
 # warm
 # THEME=sexy-sweetlove
 
 # cool
-THEME=sexy-navy-and-ivory
-wal -o $HOME/py/polybar-themer/use_pywal_theme.py -o dunst.sh -o launcher.sh --theme $THEME 
+# THEME=sexy-navy-and-ivory
+THEME=base16-atelier-sulphurpool
+# THEME=sexy-gotham
+## metallic
+# THEME=base16-black-metal-nile
+# THEME=base16-black-metal-burzum
+wal --saturate 0.2 -o theme-post.sh --theme ${THEME:-random}
