0ce265df80dc5520bbf437dae05d6e677f42d99b
Hugo Rivera <6373447+roguh@users.noreply.github.com>
2018-10-05 02:19:20 -0600

    Update pywal

--- pywal/bin/wal-set-theme.sh
+++ pywal/bin/wal-set-theme.sh
@@ -7,20 +7,23 @@
 # THEME=base16-embers
 # THEME=dkeg-novmbr
 # THEME=dkeg-bark
+# THEME=dkeg-simplicity
 ## metallic
 # THEME=base16-black-metal-nile
 # THEME=dkeg-urban
 
 # cool
-THEME=base16-flat
+# THEME=sexy-vacuous2
+# THEME=base16-flat
+# THEME=base16-onedark
 # THEME=base16-atelier-lakeside
 # THEME=hybrid-material
 # THEME=sexy-navy-and-ivory
 # THEME=base16-atelier-sulphurpool
-# THEME=sexy-gotham
+THEME=sexy-gotham
 # THEME=sexy-jasonwryan
 # THEME=dkeg-prevail
 ## metallic
 # THEME=base16-black-metal-burzum
 # THEME=dkeg-blumune
-wal --saturate 0.2 -o theme-post.sh --theme ${THEME:-random}
+wal -o theme-post.sh --theme ${THEME:-random}
