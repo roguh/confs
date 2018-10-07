af1e0ee6f74eb1fbf0e8b1b5424c80bd48a4d9b3
Hugo Rivera <6373447+roguh@users.noreply.github.com>
2018-10-07 02:47:26 -0600

    Update pywal

--- pywal/bin/wal-set-theme.sh
+++ pywal/bin/wal-set-theme.sh
@@ -3,7 +3,7 @@
 # THEME=base16-zenburn
 
 # warm
-# THEME=sexy-sweetlove
+THEME=sexy-sweetlove
 # THEME=base16-embers
 # THEME=dkeg-novmbr
 # THEME=dkeg-bark
@@ -13,6 +13,8 @@
 # THEME=dkeg-urban
 
 # cool
+# THEME=sexy-thwump # pale
+# THEME=base16-oceanicnext
 # THEME=sexy-vacuous2
 # THEME=base16-flat
 # THEME=base16-onedark
@@ -20,7 +22,7 @@
 # THEME=hybrid-material
 # THEME=sexy-navy-and-ivory
 # THEME=base16-atelier-sulphurpool
-THEME=sexy-gotham
+# THEME=sexy-gotham
 # THEME=sexy-jasonwryan
 # THEME=dkeg-prevail
 ## metallic
