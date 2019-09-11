d4424fa32e833edaadad902ec0348d2c093b2813
Hugo Rivera <6373447+roguh@users.noreply.github.com>
2019-09-11 13:18:09 -0700

    Update cerberus

--- performance/bin/cerberus.sh
+++ performance/bin/cerberus.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-_NO_COLOR="true"
-_GIT_AUTHOR="\(<torvalds@linux-foundation.org>\)\|\(<6373447+roguh@users.noreply.github.com>\)"
-_GIT_SINCE="2019-01-01"
-watch -n5 ' ./git-quick-stats --detailed-git-stats'
+export _NO_COLOR="true"
+export _GIT_AUTHOR="\(<torvalds@linux-foundation.org>\)\|\(<6373447+roguh@users.noreply.github.com>\)"
+# _GIT_SINCE="2019-01-01"
+git-quick-stats --detailed-git-stats
