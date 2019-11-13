d74f8479c90226e64edbf4972830c684519449bc
Hugo Rivera <6373447+roguh@users.noreply.github.com>
2019-11-13 12:19:05 -0800

    fish window title

--- fish_the_best_sh/.config/fish/functions/fish_title.fish
+++ fish_the_best_sh/.config/fish/functions/fish_title.fish
@@ -1,4 +1,18 @@
-function fish_title
-  echo "🐟 \$" (pwd | trimdir.py)
+function title_get_project
+  git rev-parse 2> /dev/null
+  if test "$status" = 0
+    printf "| git:%s" (basename (git rev-parse --show-toplevel 2> /dev/null) "")
+  end
 end
 
+function title_trimdir
+  if type trimdir.py > /dev/null 2>&1
+    trimdir.py (pwd)
+  else
+    pwd
+  end
+end
+
+function fish_title
+  echo -e "🐟 \$" (title_trimdir) " " (title_get_project)
+end
