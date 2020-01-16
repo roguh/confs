3f8821c9300c0092b6a6b042b7feada5c69062f8
Hugo O. Rivera <6373447+roguh@users.noreply.github.com>
2020-01-15 19:30:11 -0800

    Add feh with sane defaults

--- update.sh
+++ update.sh
@@ -194,3 +194,5 @@ copy_confs_for fanciness bin/showoff-linux-desktop-unixporn-sexy-oh-wow-screensh
 
 # git@github.com:roguh/git-quick-stats.git
 copy_confs_for performance bin/cerberus.sh bin/cerberus-notify-send.sh
+
+copy_confs_for feh_sane_defaults feh-sane-defaults.sh
