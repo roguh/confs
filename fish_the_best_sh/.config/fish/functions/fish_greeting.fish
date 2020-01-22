1f2462e0b4f990096204edb216dbe2f0c8778743
Hugo O. Rivera <6373447+roguh@users.noreply.github.com>
2020-01-22 10:55:31 -0800

    Update greeting

--- fish_the_best_sh/.config/fish/functions/fish_greeting.fish
+++ fish_the_best_sh/.config/fish/functions/fish_greeting.fish
@@ -1,18 +1,36 @@
 function fish_greeting
 	echo Welcome to fish, the FRIENDLY INTERACTIVE SHELL. 🐠
-echo Type `sl` for instructions on how to use fish. 🐠
-echo 🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠
-echo
-echo
-echo
-
-echo (curl https://raw.githubusercontent.com/asdf-vm/asdf/master/ballad-of-asdf.md)
-
-echo (hostname) '>' ls $PWD
-ls $PWD
-if type fortune > /dev/null 2>>&1
-fortune
-else
-echo FOTD: Keep your house clean.
-end
+  echo Type `sl` for instructions on how to use fish. 🐠
+  echo 🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠🐠
+  echo
+  echo
+  echo
+  
+  if [ "$PWD" = "$HOME" ]
+    screenfetch
+    
+    echo
+    echo
+    
+    echo (curl https://raw.githubusercontent.com/asdf-vm/asdf/master/ballad-of-asdf.md)
+    
+    echo
+    echo
+  end
+  
+  echo (hostname) '>' ls "$PWD"
+  if type exa > /dev/null 2>>&1
+    exa "$PWD"
+  else
+    ls "$PWD"
+  end
+  
+  echo
+  echo
+  
+  if type fortune > /dev/null 2>>&1
+    fortune
+  else
+    echo FOTD: Keep your house clean.
+  end
 end
