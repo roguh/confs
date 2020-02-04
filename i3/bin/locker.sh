0b94636d24ccd41a0ec7f33e7b2d24b139f8a872
Hugo O. Rivera <hugo@roguh.com>
2020-02-04 11:14:15 -0800

    Use blurlock

--- i3/bin/locker.sh
+++ i3/bin/locker.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -
 backlightoff.sh &
-exec i3lock -c ff0000
+# exec i3lock -c ff0000
 
-# if command -v blurlock > /dev/null; then
-#   blurlock
-# else
-#   # -sample is faster than -scale or -resize
-#   scrot $HOME/tmp/s.png && \
-#   mogrify -sample 50% -spread 1 -paint 2 -normalize -sample 200% $HOME/tmp/s.png && \
-#   i3lock --ignore-empty-password --show-failed-attempts -c 111111 --image="$HOME/tmp/s.png"
-# fi
+if command -v blurlock > /dev/null; then
+  blurlock
+else
+  # -sample is faster than -scale or -resize
+  scrot $HOME/tmp/s.png && \
+  mogrify -sample 50% -spread 1 -paint 2 -normalize -sample 200% $HOME/tmp/s.png && \
+  i3lock --ignore-empty-password --show-failed-attempts -c 111111 --image="$HOME/tmp/s.png"
+fi
