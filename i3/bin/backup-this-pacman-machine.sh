c7f77dcab513a15828ead8f804d43c135257c82b
Hugo O. Rivera <hugo@roguh.com>
2021-10-06 10:18:15 -0700

    Add ability to run script anywhere

--- i3/bin/backup-this-pacman-machine.sh
+++ i3/bin/backup-this-pacman-machine.sh
@@ -12,7 +12,7 @@ rsync --relative $RSYNC_OPTS $(pacman -Qii | awk '/^MODIFIED/ {print $2}') $ROOT
 pacman -Qie --native > $ROOT/pacman-Qie
 pacman -Qie --foreign > $ROOT/pacman-Qie-AUR
 
-cp .local/share/fish/fish_history $ROOT/fish_history
+cp ~/.local/share/fish/fish_history $ROOT/fish_history
 
 rsync $RSYNC_OPTS $HOME/bin/ $ROOT/bin
 rsync $RSYNC_OPTS $HOME/.screenlayout/ $ROOT/dotscreenlayout
