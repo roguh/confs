cbbec593bc25ce3c1bcef70c409a2e52c703ce52
Hugo O. Rivera <hugo@roguh.com>
2021-10-07 10:13:44 -0600

    More style

--- i3/bin/launcher.sh
+++ i3/bin/launcher.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -
 if type rofi >/dev/null 2>&1
 then
-    rofi -padding 50 -show ${1:-run} -modi run,window,keys -sidebar-mode -lines 50 -disable-history
+    rofi -padding 50 -show ${1:-run} -modi run,window,keys -sidebar-mode -lines 25 -disable-history
 else
     dmenu_run
 fi
