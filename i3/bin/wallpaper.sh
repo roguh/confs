7bb456c9884beb15529d9bc5313a2ad9cb8615f9
Hugo O. Rivera <hugo@roguh.com>
2021-11-18 15:30:59 -0800

    Update wallpaper.sh

--- i3/bin/wallpaper.sh
+++ i3/bin/wallpaper.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-DIR=$HOME/Dropbox/sync/media/wallpaper/
-if [ -f $DIR/$(hostname).png ]; then
-  feh --bg-tile $DIR/$(hostname).png
-elif [ -f $DIR/$(hostname).jpg ]; then
-  feh --bg-tile $DIR/$(hostname).jpg
-else
-  feh --bg-tile $DIR/Red-Roses-Pattern.png
+FEH_OPTS=--bg-tile
+if [ -f "$DIR/$(hostname).feh_opts" ]; then
+  FEH_OPTS="$(cat "$DIR/$(hostname).feh_opts")"
 fi
+
+DIR="$HOME/Dropbox/sync/media/wallpaper/"
+for ext in jpg jpeg png; do
+  if [ -f "$DIR/$(hostname).$ext" ]; then
+    feh $FEH_OPTS "$DIR/$(hostname).$ext"
+    exit 0
+  fi
+done
+
+feh $FEH_OPTS "$DIR/Red-Roses-Pattern.png"
