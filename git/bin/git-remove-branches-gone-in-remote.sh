0555a3499e60ebc571b2d7343d2b39afa85d9406
Hugo O. Rivera <hugo@roguh.com>
2021-12-14 09:09:00 -0800

    Improve git script git-remove-branches-gone-in-remote.sh

--- git/bin/git-remove-branches-gone-in-remote.sh
+++ git/bin/git-remove-branches-gone-in-remote.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 MUST_DELETE="$(git fetch -p && git branch -vv | sed 's/\* *//' | awk '/: gone]/{print $1}')"
 
+
+FORCE_DELETE=${FORCE_DELETE-false}
+if [ "$1" = "-f" ]; then
+  FORCE_DELETE=true
+fi
+
 if [ "$MUST_DELETE" = "" ]; then
   echo No branches to delete
   exit
@@ -9,8 +15,12 @@ fi
 echo Press ENTER to delete:
 echo "$MUST_DELETE"
 read -r OK
-if [[ "$OK" != "" ]] ; then
+if ! [ "$OK" = "" ] ; then
   exit 1
 fi
 
-echo "$MUST_DELETE" | xargs git branch -d
+if [ "$FORCE_DELETE" = true ]; then
+  echo "$MUST_DELETE" | xargs git branch -D
+else
+  echo "$MUST_DELETE" | xargs git branch -d
+fi
