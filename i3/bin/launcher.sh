a7b56d718c4351404d1cb6a591d7371030ffb97c
Hugo O. Rivera <hugo@roguh.com>
2021-12-30 19:16:56 -0700

    rofi fuzzy

--- i3/bin/launcher.sh
+++ i3/bin/launcher.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -
-if type rofi >/dev/null 2>&1
+if command -v rofi > /dev/null
 then
-    rofi -padding 50 -show ${1:-run} -modi run,window,keys -sidebar-mode -lines 25 -disable-history
+    if command -v rofi > /dev/null
+    then
+        rofi -padding 50 -show ${1:-run} -modi run,window,keys -sidebar-mode -lines 25 -disable-history -matching fuzzy
+    else
+        rofi -padding 50 -show ${1:-run} -modi run,window,keys -sidebar-mode -lines 25 -disable-history -matching fuzzy -sorting-method fzf
+    fi
 else
     dmenu_run
 fi
