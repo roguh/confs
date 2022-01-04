68f0a93d21e398515c7d00431ba86b6c2df98799
Hugo O. Rivera <hugo@roguh.com>
2022-01-04 10:27:55 -0700

    Add keyboard layout listing comments

--- host-specific/Tezcatlipoca-T580/x11_keyboard_layout/bin/set-keyboard-layout.sh
+++ host-specific/Tezcatlipoca-T580/x11_keyboard_layout/bin/set-keyboard-layout.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# localectl list-x11-keymap-layouts
+# localectl list-x11-keymap-variants LAYOUT_CODE
+
 setxkbmap -layout us,ru
 # Unset variant to use official russian keyboard, instead of english qwerty analogue
 setxkbmap -variant ,phonetic
