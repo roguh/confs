e4b5c18d912316d1de41a4b2b3f56ec656292d97
Hugo O. Rivera <hugo@roguh.com>
2022-01-08 12:17:22 -0700

    Update rofi launcher options

--- i3/bin/launcher.sh
+++ i3/bin/launcher.sh
@@ -1,12 +1,7 @@
 #!/bin/bash -
 if command -v rofi > /dev/null
 then
-    if command -v rofi > /dev/null
-    then
-        rofi -padding 50 -show ${1:-run} -modi run,window,keys -sidebar-mode -lines 25 -disable-history -matching fuzzy
-    else
-        rofi -padding 50 -show ${1:-run} -modi run,window,keys -sidebar-mode -lines 25 -disable-history -matching fuzzy -sorting-method fzf
-    fi
+    rofi -padding 50 -show ${1:-run} -modi run,window,keys -sidebar-mode -lines 25 -sorting-method fzf
 else
     dmenu_run
 fi
