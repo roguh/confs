c67e24c91c107dfee5c752e2afbebd659bcf7ba1
Hugo O. Rivera <hugo@roguh.com>
2022-03-08 16:02:37 -0700

    Host specific files updates

--- host-specific/Tezcatlipoca-T580/x11_keyboard_layout/bin/set-keyboard-layout.sh
+++ host-specific/Tezcatlipoca-T580/x11_keyboard_layout/bin/set-keyboard-layout.sh
@@ -2,7 +2,7 @@
 # localectl list-x11-keymap-layouts
 # localectl list-x11-keymap-variants LAYOUT_CODE
 
-setxkbmap -layout us,ru
+setxkbmap -layout us,ru,es,fr
 # Unset variant to use official russian keyboard, instead of english qwerty analogue
-setxkbmap -variant ,phonetic
+setxkbmap -variant ,phonetic,,
 setxkbmap -option 'grp:ctrls_toggle'
