35b8d26492e444a5190ff7dfef0b75177318b508
Hugo O. Rivera <hugo@roguh.com>
2023-04-08 04:51:46 -0600

    i3

---
+++ i3/bin/nvidia-i3-status.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -o pipefail
+
+if ! nvidia-smi --query-gpu=memory.used --format=csv > /dev/zero ; then
+  exit 1
+fi
+
+set -x
+vrams=$(nvidia-smi --query-gpu=memory.used --format=csv,noheader,nounits | awk '{printf "%.2fGB ", $1 / 1024}')
+temps=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits | awk '{printf "%.0f°F ", (($1 * 1.8) + 32)}')
+
+echo "$vrams $temps"
+
