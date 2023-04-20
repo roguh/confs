2512831c4f40603447ef4561f84fd26189b56334
Hugo O. Rivera <hugo@roguh.com>
2023-04-20 02:11:26 -0600

    Update feh

--- feh_sane_defaults/bin/feh-sane-defaults.sh
+++ feh_sane_defaults/bin/feh-sane-defaults.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-feh --auto-reload --auto-zoom --borderless --draw-exif --draw-filename --draw-tinted --force-aliasing --fullscreen --hide-pointer --image-bg checks --no-screen-clip --preload --scroll-step 100 --sort filename --zoom "$@"
+feh --auto-zoom --borderless --draw-exif --draw-filename --draw-tinted --force-aliasing --fullscreen --hide-pointer --image-bg checks --no-screen-clip --preload --scroll-step 100 --sort filename --zoom "$@"
